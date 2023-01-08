75c256ae701c0ac8a8b89f2117da9da0a6b28075
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-08 15:57:01 -0800

    Test: dish calorie count method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-  describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :description}
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
   end
-  describe "relationships" do
-    it {should belong_to :chef}
-    it {should have_many :recipes}
-    it {should have_many(:ingredients).through(:recipes)}
+  describe 'relationships' do
+    it { should belong_to :chef }
+    it { should have_many :recipes }
+    it { should have_many(:ingredients).through(:recipes) }
+  end
+  describe 'instance methods' do
+    it '#calories returns calorie count for dish' do
+      remy = Chef.create!(name: 'Remy')
+      ratatouille = remy.dishes.create!(name: 'Ratatouille', description: 'Stewed summer vegetables')
+      eggplant = Ingredient.create!(name: 'Eggplant', calories: 100)
+      zucchini = Ingredient.create!(name: 'Zucchini', calories: 90)
+      evoo = Ingredient.create!(name: 'Extra Virgin Olive Oil', calories: 300)
+      recipe1 = Recipe.create!(ingredient_id: eggplant.id, dish_id: ratatouille.id)
+      recipe2 = Recipe.create!(ingredient_id: zucchini.id, dish_id: ratatouille.id)
+      recipe3 = Recipe.create!(ingredient_id: evoo.id, dish_id: ratatouille.id)
+
+      expect(ratatouille.calories).to eq(490)
+    end
   end
 end
