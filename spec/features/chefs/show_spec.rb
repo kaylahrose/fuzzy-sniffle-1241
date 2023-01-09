e8bad9427484409d26b557b7e7ac114857680181
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-08 16:23:06 -0800

    Test: link to chef ingredients list

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -16,12 +16,27 @@ RSpec.describe 'Chef show page' do
     ratatouille = remy.dishes.create!(name: 'Ratatouille', description: 'Stewed summer vegetables')
 
     visit chef_path(remy)
-    
+
     fill_in 'Name', with: 'Croissant'
     fill_in 'Description', with: 'flaky butter pastry with layers'
     click_button 'Create Dish'
 
     expect(current_path).to eq(chef_path(remy))
     expect(page).to have_content('Croissant')
+  end
+
+#   As a visitor
+# When I visit a chef's show page
+# I see a link to view a list of all ingredients that this chef uses in their dishes.
+# When I click on that link
+# I'm taken to a chef's ingredients index page
+# and I can see a unique list of names of all the ingredients that this chef uses.
+  it 'link to chef ingredient index' do
+    remy = Chef.create!(name: 'Remy') 
+    visit chef_path(remy)
+
+    click_link "View ingredients list"
+
+    expect(current_path).to eq(chef_ingredients_path(remy))
   end
 end
